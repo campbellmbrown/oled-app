d4736deb5b67edfe1d88fc2bc9f5261ef6a839cb
campbellmbrown <campbellbrown093@gmail.com>
2025-09-11 18:46:55 +1200

    VS Code settings

--- src/oled_driver.cpp
+++ src/oled_driver.cpp
@@ -2,7 +2,7 @@
 
 #include <U8g2lib.h>
 
-static U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+static U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock */ SCL, /* data */ SDA, /* reset */ U8X8_PIN_NONE);
 
 #define OLED_WIDTH           (128)
 #define OLED_HEIGHT          (64)
