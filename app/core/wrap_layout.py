ed56eed71b06a4552e4c51ceeff1d3976ba2a537
campbellmbrown <campbellbrown093@gmail.com>
2025-09-12 23:39:17 +1200

    Reducing mypy errors

--- app/core/wrap_layout.py
+++ app/core/wrap_layout.py
@@ -1,16 +1,16 @@
 from PySide6.QtCore import QPoint, QRect, QSize, Qt
-from PySide6.QtWidgets import QLayout, QLayoutItem, QSizePolicy
+from PySide6.QtWidgets import QLayout, QLayoutItem, QSizePolicy, QWidget
 
 
 class WrapLayout(QLayout):
-    def __init__(self, parent=None, margin: int = 0, hspacing: int = 0, vspacing: int = 0) -> None:
+    def __init__(self, parent: QWidget | None = None, margin: int = 0, hspacing: int = 0, vspacing: int = 0) -> None:
         super().__init__(parent)
         self.hspacing = hspacing
         self.vspacing = vspacing
         self.items: list[QLayoutItem] = []
         self.setContentsMargins(margin, margin, margin, margin)
 
-    def __del__(self):
+    def __del__(self) -> None:
         del self.items[:]
 
     def addItem(self, item: QLayoutItem) -> None:
@@ -28,8 +28,9 @@ class WrapLayout(QLayout):
     def itemAt(self, index: int) -> QLayoutItem | None:
         if 0 <= index < len(self.items):
             return self.items[index]
+        return None
 
-    def takeAt(self, index: int) -> QLayoutItem | None:
+    def takeAt(self, index: int) -> QLayoutItem:
         if 0 <= index < len(self.items):
             return self.items.pop(index)
 
@@ -63,6 +64,10 @@ class WrapLayout(QLayout):
         content_margins = self.getContentsMargins()
         assert isinstance(content_margins, tuple)
         left, top, right, bottom = content_margins
+        assert isinstance(left, int)
+        assert isinstance(top, int)
+        assert isinstance(right, int)
+        assert isinstance(bottom, int)
         effective = rect.adjusted(+left, +top, -right, -bottom)
         x = effective.x()
         y = effective.y()
